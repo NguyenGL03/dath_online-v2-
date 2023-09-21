01c4b7e550ab9f8eed7dbb95f29c8d4d926a20e3
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-09-21 19:57:17 +0700

    Tính năng thay đổi thứ tự sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -35,10 +35,9 @@ module.exports.index = async (req, res) => {
   // End Pagination
 
   const products = await Product.find(find)
+    .sort({ position: "desc" })
     .limit(objectPagination.limitItems)
     .skip(objectPagination.skip);
-
-  console.log(products);
 
   if(products.length > 0) {
     res.render("admin/pages/products/index", {
@@ -88,6 +87,12 @@ module.exports.changeMulti = async (req, res) => {
         deleted: true,
         deletedAt: new Date()
       });
+      break;
+    case "change-position":
+      for (const item of ids) {
+        const [id, position] = item.split("-");
+        await Product.updateOne({ _id: id }, { position: position });
+      }
       break;
     default:
       break;
