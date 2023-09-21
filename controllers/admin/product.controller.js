99665c5fa369f55158a8544bd58bd43b8f4f19b8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-09-21 19:22:48 +0700

    1. Tính năng xóa nhiều sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -83,6 +83,12 @@ module.exports.changeMulti = async (req, res) => {
     case "inactive":
       await Product.updateMany({ _id: {$in: ids} }, { status: type });
       break;
+    case "delete-all":
+      await Product.updateMany({ _id: {$in: ids} }, {
+        deleted: true,
+        deletedAt: new Date()
+      });
+      break;
     default:
       break;
   }
