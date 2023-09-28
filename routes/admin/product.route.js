5703dc681963729ac9a20792556ef1feef331d70
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-09-28 19:26:16 +0700

    1. Validate dữ liệu

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -8,6 +8,7 @@ const storage = storageMulterHelper();
 const upload = multer({ storage: storage });
 
 const controller = require("../../controllers/admin/product.controller");
+const validate = require("../../validates/admin/product.validate");
 
 router.get("/", controller.index);
 
@@ -19,6 +20,11 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", upload.single("thumbnail"), controller.createPost);
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.createPost
+);
 
 module.exports = router;
