307309010a9c4b798dacdb0f4058af3e5b038f6f
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-10-10 20:42:12 +0700

    2. Thêm logs deletedBy

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -120,7 +120,11 @@ module.exports.changeMulti = async (req, res) => {
         { _id: { $in: ids } },
         {
           deleted: true,
-          deletedAt: new Date(),
+          // deletedAt: new Date(),
+          deletedBy: {
+            account_id: res.locals.user.id,
+            deletedAt: new Date(),
+          }
         }
       );
       req.flash("success", `Xóa thành công ${ids.length} sản phẩm!`);
@@ -148,7 +152,11 @@ module.exports.deleteItem = async (req, res) => {
     { _id: id },
     {
       deleted: true,
-      deletedAt: new Date(),
+      // deletedAt: new Date(),
+      deletedBy: {
+        account_id: res.locals.user.id,
+        deletedAt: new Date(),
+      }
     }
   );
 
